e3efbc6f2353e0cf5ad0461f3a23a885925f5087
Erik Nelson <eanelson@eecs.berkeley.edu>
2015-10-17 16:45:40 -0700

    Adding documentation.

---
+++ documentation/search/functions_d.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['naivefeaturematcher',['NaiveFeatureMatcher',['../classbsfm_1_1NaiveFeatureMatcher.html#aa10ada0708a01407833449744d15eae1',1,'bsfm::NaiveFeatureMatcher']]]
+];

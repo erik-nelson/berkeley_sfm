067e1177c49df1e6ab35799ddabc5ac590a34ee3
erik-nelson <eanelson@eecs.berkeley.edu>
2015-10-18 03:12:40 +0000

    (1 of 2) Deleting documentation. Automated documentation build for changeset 2356e8ead9ab4ba423a1c9cd5a6a508518233d8e.

--- documentation/search/typedefs_63.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['constptr',['ConstPtr',['../classbsfm_1_1Image.html#a0cc0f347a13bf5df7f3d371fe8cb14ec',1,'bsfm::Image::ConstPtr()'],['../structbsfm_1_1Feature.html#a9e26b4d92937189b7905fe6d5890586b',1,'bsfm::Feature::ConstPtr()'],['../structbsfm_1_1FeatureMatch.html#a2277b16cea7ddf745fdb15b491391c5d',1,'bsfm::FeatureMatch::ConstPtr()'],['../structbsfm_1_1LightFeatureMatch.html#a5cda65a06fccff758ccc2e6d52c2ab4b',1,'bsfm::LightFeatureMatch::ConstPtr()'],['../structbsfm_1_1PairwiseImageMatch.html#a0da3a1f1376f048ca6cc3a9715ed9e20',1,'bsfm::PairwiseImageMatch::ConstPtr()']]]
-];

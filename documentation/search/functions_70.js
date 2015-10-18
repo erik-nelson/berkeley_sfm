3fb055398ddeb7daaeda92e744f5b1a4fead2345
erik-nelson <eanelson@eecs.berkeley.edu>
2015-10-18 03:12:43 +0000

    (2 of 2) Adding new documentation. Automated documentation build for changeset 2356e8ead9ab4ba423a1c9cd5a6a508518233d8e.

---
+++ documentation/search/functions_70.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['pathsequivalent',['PathsEquivalent',['../namespacestrings.html#a15f7a1478a39f103728bb6c4c766591f',1,'strings']]],
+  ['pointinimage',['PointInImage',['../classbsfm_1_1CameraIntrinsics.html#a33590c63264773ab632380a1dbc92e6c',1,'bsfm::CameraIntrinsics']]],
+  ['pose',['Pose',['../classbsfm_1_1Pose.html#a7c25963e2248854857bfb81a1f46b334',1,'bsfm::Pose::Pose()'],['../classbsfm_1_1Pose.html#abad6ab4db45565b485ed07aa3161b673',1,'bsfm::Pose::Pose(const Eigen::Matrix3d &amp;R, const Eigen::Vector3d &amp;t)'],['../classbsfm_1_1Pose.html#a8acd8622d61ff2e0260eb72c692678c5',1,'bsfm::Pose::Pose(const Pose &amp;other)'],['../classbsfm_1_1Pose.html#a95d72491e102061f42d27058a4856cf4',1,'bsfm::Pose::Pose(const Eigen::Matrix4d &amp;)']]],
+  ['prependslash',['PrependSlash',['../namespacestrings.html#abd916bb56999634dae0a5b51eb073192',1,'strings']]],
+  ['print',['Print',['../classbsfm_1_1Pose.html#a7837425c284fcd9b68e7cc660e31eaab',1,'bsfm::Pose']]],
+  ['project',['Project',['../classbsfm_1_1Pose.html#a35341e34d1e2d888299d726da8587c69',1,'bsfm::Pose']]],
+  ['projectto2d',['ProjectTo2D',['../classbsfm_1_1Pose.html#a8312d0f628fc3d22cf9daa631c335186',1,'bsfm::Pose']]]
+];
---
+++ documentation/search/typedefs_6b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['keypoint',['Keypoint',['../namespacebsfm.html#ae142f8a0e67c9d734ebf8fea62d6261f',1,'bsfm']]],
+  ['keypointlist',['KeypointList',['../namespacebsfm.html#a86295c95313310a7b5a433a0524ebc7f',1,'bsfm']]]
+];

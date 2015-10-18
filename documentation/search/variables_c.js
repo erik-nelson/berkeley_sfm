50cc8cbd29e3383188a32c60ff1f96e8216dfdb1
erik-nelson <eanelson@eecs.berkeley.edu>
2015-10-18 03:12:35 +0000

    (1 of 2) Deleting documentation. Automated documentation build for changeset 2356e8ead9ab4ba423a1c9cd5a6a508518233d8e.

--- documentation/search/variables_c.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['normalize_5ffeatures',['normalize_features',['../structbsfm_1_1FundamentalMatrixSolverOptions.html#aff05a1355658a9fd9fb9106c907e0ab3',1,'bsfm::FundamentalMatrixSolverOptions']]],
-  ['num_5fbest_5fmatches',['num_best_matches',['../structbsfm_1_1FeatureMatcherOptions.html#a71f9beb03aaa935264423eacd5fe7119',1,'bsfm::FeatureMatcherOptions']]],
-  ['num_5fsamples',['num_samples',['../structbsfm_1_1RansacOptions.html#a1b9808d7515490cbb8fd569b0b9cb5f6',1,'bsfm::RansacOptions']]]
-];

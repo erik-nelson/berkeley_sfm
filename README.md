002b70a00b8bc1f43439dbb667f46761067d2369
Erik Nelson <eanelson@eecs.berkeley.edu>
2015-10-18 17:33:40 -0700

    now runs tests. This removes need to switch directories for testing. Test executable is compiled under build/run_tests

--- README.md
+++ README.md
@@ -6,13 +6,19 @@
 Berkeley SfM is a structure from motion library created to explore new solutions to challenging 3D reconstruction problems (large scale, indoors, incremental, homogeneously textured, etc.). It is developed by **Erik Nelson** and **David Fridovich-Keil**, members of the Berkeley Vision and Learning Center ([BVLC](http://bvlc.eecs.berkeley.edu)).
 
 ## Overview
-This repository is entirely written in C++, and is structured around the CMAKE cross-compilation paradigm. All source code is in the `berkeley_sfm/src/cpp/` directory. We have also written extensive unit tests, which are automatically compiled and tested in continuous integration. The source code for these tests is stored in the `berkeley_sfm/test/` directory, and the test executable will be compiled into `berkeley_sfm/bin/run_tests`.
+This repository is entirely written in C++, and is structured around the CMAKE cross-compilation paradigm. All source code is in the `berkeley_sfm/src/cpp/` directory. We have also written extensive unit tests, which are automatically compiled and tested in continuous integration. The source code for these tests is stored in the `berkeley_sfm/test/` directory, and the test executable will be compiled into `berkeley_sfm/build/run_tests`. Tests can be run using `make check`.
 
 ## Build Instructions
 We follow the standard CMAKE build scheme. Just download the repository and from the top directory type:
 
 ```bash
 mkdir build && cd build && cmake .. && make
+```
+
+Optionally, to run tests:
+
+```bash
+make check
 ```
 
 We have tested the build process on Mac OSX Yosemite and El Capitan, and on Ubuntu 14.04 Trusty. One difficulty we did encounter is the potential conflict between multiple copies of OpenCV on a system. For example, if you run Anaconda's (and/or Homebrew's) version of OpenCV it may not be compiled properly such that it will link to our repository. If you run into this problem, we suggest uninstalling all versions of OpenCV and rebuilding it from source with the default compiler on your system (not Homebrew's version of gcc/g++).
--- documentation/publish_documentation.sh
+++ documentation/publish_documentation.sh
@@ -3,6 +3,9 @@
 # Quit on errors.
 set -o errexit -o nounset
 
+# Get to the top directory.
+cd ..
+
 # Settings.
 DOCUMENTATION_PATH=documentation
 CHANGESET=$(git rev-parse --verify HEAD)
@@ -21,7 +24,7 @@ git fetch origin
 # Commit new documentation from master branch.
 touch ${DOCUMENTATION_PATH}
 git add ${DOCUMENTATION_PATH}
-git commit -m "Automated documentation for changeset ${CHANGESET}."
+git commit -m ""
 
 # Check out gh-pages branch and merge documentation from master commit.
 git checkout -b gh-pages origin/gh-pages
@@ -30,51 +33,7 @@ git checkout master ${DOCUMENTATION_PATH}
 
 # Add the merged changes and push.
 git add -A
-git commit -m "Automated documentation for changeset ${CHANGESET}."
+git commit -m "Automated documentation build for changeset ${CHANGESET}."
 git push -u origin gh-pages
 
 echo "-- Successfully updated documentation!"
-
-#git init
-#git remote add upstream "https://$PA_TOKEN@github.com/${COMMIT_USER}/berkeley_sfm.git"
-#git fetch upstream
-#git reset upstream/gh-pages
-
-#touch ${DOCUMENTATION_PATH}
-#git add ${DOCUMENTATION_PATH}
-#git commit -m "Automated documentation build for changeset ${CHANGESET}."
-#git push -q upstream HEAD:gh-pages
-
-
-
-
-## Remove stale documentation.
-#git checkout -b gh-pages origin/gh-pages
-#if [ -d "${HTML_PATH}" ]; then
-#  # Delete all documentation and push.
-#  cd ${HTML_PATH}
-#  git rm -rf ./*
-#  cd ..
-#  git commit -a -m "Automated documentation for changeset ${CHANGESET}: step 1 of 2 -  Deleting old documentation."
-#  git push origin gh-pages
-#fi
-#echo "-- Successfully removed stale documentation."
-
-## Make the documentation from source files on the master branch.
-#git checkout master
-#if [ -d "${BUILD_PATH}" ]; then
-#  cd ${BUILD_PATH}
-#  make documentation
-#  cd -
-
-#  # Checkout the gh-pages branch, transferring all documentation.
-#  git checkout gh-pages
-
-#  # Publish the documentation.
-#  cd ${HTML_PATH}
-#  git add .
-#  git commit -m "Automated documentation for changeset ${CHANGESET}: step 2 of 2 - Adding new documentation."
-#  git push origin gh-pages
-#  cd -
-#fi
-#echo "-- Successfully updated documentation!"

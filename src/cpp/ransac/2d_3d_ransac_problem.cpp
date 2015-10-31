2371cf4a9154f3d726ca4003388ac18c2569e083
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-10-31 12:57:02 -0700

    finished 2d_3d_ransac -- need to write separate method to do brute force matching to generate Observations from new view

---
+++ src/cpp/ransac/2d_3d_ransac_problem.cpp
@@ -0,0 +1,203 @@
+/*
+ * Copyright (c) 2015, The Regents of the University of California (Regents).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *    3. Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Please contact the author(s) of this library if you have any questions.
+ * Authors: David Fridovich-Keil   ( dfk@eecs.berkeley.edu )
+ *          Erik Nelson            ( eanelson@eecs.berkeley.edu )
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// These classes define the 2D3DRansacProblem API, and derive from
+// the base RansacProblem and RansacModel class/struct.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "2d_3d_ransac_problem.h"
+
+namespace bsfm {
+
+// ------------ 2D3DRansacModel methods ------------ //
+
+// Default constructor. Initialize to empty camera.
+PnPRansacModel::PnPRansacModel() {
+  camera_ = Camera();
+  error_ = 0.0;
+}
+
+// Constructor. Set parameters as given.
+PnPRansacModel::PnPRansacModel(const Camera& camera,
+			       std::vector<Observation::Ptr>& matches)
+  : camera_(camera),
+    matches_(matches),
+    error_ = 0.0 {}
+
+// Destructor.
+PnPRansacModel::~PnPRansacModel() {}
+
+// Return model error.
+double PnPRansacModel::Error() const {
+  return error_;
+}
+
+// Evaluate model on a single data element and update error.
+bool PnPRansacModel::IsGoodFit(const Observation::Ptr observation,
+			       double error_tolerance) {
+  // Get error.
+  double error = PnPRansacModel::EvaluateReprojectionError(observation);
+
+  // Did not project into the image.
+  if (error < 0.0)
+    return false;
+
+  // Check tolerance.
+  if (error <= error_tolerance)
+    return true;
+  return false;
+}
+
+// Compute reprojection error of this landmark. Return -1.0 if point
+// does not project into the image.
+double PnPRansacModel::EvaluateReprojectionError(
+   const Observation::Ptr observation) const {
+
+  CHECK_NOTNULL(observation.get());
+  
+  // Unpack this observation (extract Feature and Landmark).
+  Feature feature = observation->Feature();
+  Landmark::Ptr landmark = observation->GetLandmark();
+  CHECK_NOTNULL(landmark.get());
+  
+  // Extract position of this landmark.
+  Point3D point = landmark->Position();
+  
+  // Project into this camera.
+  double u = 0.0, v = 0.0;
+  const bool in_camera =
+    camera_.WorldToImage(point.X(), point.Y(), point.Z(), &u, &v);
+
+  // Check that the landmark projects into the image.
+  if (!in_camera)
+    return -1.0;
+
+  // Compute error and return.
+  double delta_u = u - feature.u_;
+  double delta_v = v - feature.v_;
+  double error = delta_u*delta_u + delta_v*delta_v;
+  
+  return error;
+}
+    
+// ------------ PnPRansacProblem methods ------------ //
+
+// Default constructor/destructor.
+PnPRansacProblem::PnPRansacProblem() {}
+PnPRansacProblem::~PnPRansacProblem() {}
+
+// Set camera intrinsics.
+PnPRansacProblem::SetIntrinsics(CameraIntrinsics& intrinsics)
+  : intrinsics_(intrinsics) {} 
+  
+// Subsample the data.
+std::vector<Observation::Ptr> PnPRansacProblem::SampleData(
+   unsigned int num_samples) {
+
+  // Randomly shuffle the entire dataset and take the first elements.
+  std::random_shuffle(data_.begin(), data_.end());
+
+  // Make sure we don't over step.
+  if (static_cast<size_t>(num_samples) > data_.size()) {
+    VLOG(1) << "Requested more RANSAC data samples than are available. "
+      "Returning all data.";
+    num_samples = data_.size();
+  }
+
+  // Get samples.
+  std::vector<Observation::Ptr> samples(
+    data_.begin(), data_.begin() + static_cast<size_t>(num_samples));
+
+  return samples;
+}
+
+// Return the data that was not sampled.
+std::vector<Observation::Ptr> RemainingData(
+    unsigned int num_sampled_previously) const {
+  // In Sample(), the data was shuffled and we took the first
+  // 'num_sampled_previously' elements. Here, take the remaining elements.
+  if (num_sampled_previously >= data_.size()) {
+    VLOG(1) << "No remaining RANSAC data to sample.";
+    return std::vector<Observation::Ptr>();
+  }
+
+  return std::vector<Observation::Ptr>(
+    data_.begin() + num_sampled_previously, data_.end());
+}
+  
+// Fit a model to the provided data using PoseEstimatorPnP.
+PnPRansacModel PnPRansacProblem::FitModel(
+    const std::vector<Observation::Ptr>& input_data) const {
+
+  // Extract a FeatureList and a Point3DList from input_data.
+  FeatureList points_2d;
+  Point3DList points_3d;
+
+  for (const auto& observation : input_data) {
+    CHECK_NOTNULL(observation.get());
+
+    // Extract feature and append.
+    points_2d.push_back(observation->Feature());
+
+    // Estract landmark position and append.
+    Landmark::Ptr landmark = observation->Landmark();
+    CHECK_NOTNULL(landmark.get());
+    points_3d.push_back(landmark->Position());
+  }
+
+  // Set up solver.
+  PoseEstimator2D3D solver;
+  solver.Initialize(points_2d, points_3d, intrinsics_);
+
+  // Solve.
+  Pose calculated_pose;
+  solver.Solve(calculated_pose);
+
+  // Generate a model from this Pose.
+  CameraExtrinsics extrinsics(calculated_pose);
+  Camera camera(extrinsics, intrinsics_);
+
+  PnPRansacModel model(camera, input_data);
+  return model;
+}
+
+} //\namespace bsfm
+
+#endif
---
+++ src/cpp/ransac/2d_3d_ransac_problem.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2015, The Regents of the University of California (Regents).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *    1. Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *    3. Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS AS IS
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Please contact the author(s) of this library if you have any questions.
+ * Authors: David Fridovich-Keil   ( dfk@eecs.berkeley.edu )
+ *          Erik Nelson            ( eanelson@eecs.berkeley.edu )
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// These classes define the 2D3DRansacProblem API, and derive from
+// the base RansacProblem and RansacModel class/struct.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef BSFM_RANSAC_2D_3D_RANSAC_PROBLEM_H
+#define BSFM_RANSAC_2D_3D_RANSAC_PROBLEM_H
+
+#include <Eigen/Dense>
+#include <vector>
+#include <glog/glog.h>
+
+#include "ransac_problem.h"
+#include "../util/disallow_copy_and_assign.h"
+#include <slam/landmark.h>
+#include <slam/observation.h>
+#include <matching/feature.h>
+#include <camera/camera_intrinsics.h>
+#include <camera/camera.h>
+#include <geometry/point_3d.h>
+
+
+namespace bsfm {
+
+using Eigen::Matrix3d;
+using Eigen::Vector3d;
+
+// ------------ PnPRansacModel derived ------------ //
+
+struct PnPRansacModel : public RansacModel<Observation::Ptr> {
+  PnPRansacModel();
+  virtual ~PnPRansacModel();
+
+  // Define an additional constructor specifically for this model.
+  PnPRansacModel(const Camera& camera, std::vector<Observation::Ptr>& matches);
+
+  // Return model error.
+  virtual double Error() const;
+
+  // Evaluate model on a single data element and update error.
+  virtual bool IsGoodFit(const Observation::Ptr observation,
+			 double error_tolerance);
+
+  // Compute reprojection error of this landmark.
+  double EvaluateReprojectionError(const Observation::Ptr observation) const;
+
+  // Model-specific member variables.
+  Camera camera_;
+  std::vector<Observation::Ptr> matches_;
+  double error_;
+};  //\struct PnPRansacModel
+
+
+// ------------ PnPRansacProblem derived ------------ //
+
+class PnPRansacProblem: public RansacProblem<Observation::Ptr, PnPRansacModel> {
+ public:
+  PnPRansacProblem();
+  virtual ~PnPRansacProblem();
+
+  // Set intrinsics.
+  void SetIntrinsics(CameraIntrinsics& intrinsics);
+
+  // Subsample the data.
+  virtual std::vector<Observation::Ptr> SampleData(unsigned int num_samples);
+
+  // Return the data that was not sampled.
+  virtual std::vector<Observation::Ptr> RemainingData(
+      unsigned int num_sampled_previously) const;
+
+  // Fit a model to the provided data using PoseEstimatorPnP.
+  virtual PnPRansacModel FitModel(
+      const std::vector<Observation::Ptr>& input_data) const;
+
+ private:
+  CameraIntrinsics intrinsics_;
+  DISALLOW_COPY_AND_ASSIGN(PnPRansacProblem)
+};  //\class PnPRansacProblem
+
+} //\namespace bsfm
+
+#endif
